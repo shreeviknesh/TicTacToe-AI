973731615d2fd21dfdd75e59d34aa4f370954803
shreeviknesh <luckyboy1998@gmail.com>
2020-04-26 01:40:53 +0530

    Tweaked AI

--- TicTacToe.h
+++ TicTacToe.h
@@ -43,6 +43,7 @@ class TicTacToe {
 private:
 	int** board;
 	std::string player1, player2;
+	int ai, human;
 
 public:
 	TicTacToe() {
@@ -60,73 +61,10 @@ public:
 
 		player1 = "Player-1";
 		player2 = "Player-2";
+		ai = State::PLAYER_TWO;
+		human = State::PLAYER_ONE;
 	}
-	// A helper function to check if the row/col/diag is same
-	int check_if_same(int x, int y, int dx, int dy) {
-		int first_field = board[x][y];
 
-		// If that first field is empty, it means that the row/col/diagonal cannot form a winner
-		if (first_field == State::EMPTY) {
-			return 0;
-		}
-
-		int i, cur_x, cur_y;
-		for (i = 0; i < 3; i++) {
-			cur_x = x + (i * dx);
-			cur_y = y + (i * dy);
-
-			if (board[cur_x][cur_y] != first_field) {
-				return 0;
-			}
-		}
-
-		// winner
-		return first_field;
-	}
-	// A function to check if any player has won, or game is tied
-	int evaluate() {
-		int result = 0;
-
-		// check all rows
-		for (int x = 0; x < 3; x++) {
-			result = check_if_same(x, 0, 0, 1);
-			if (result != 0) {
-				return result;
-			}
-		}
-
-		// check all columns
-		for (int y = 0; y < 3; y++) {
-			result = check_if_same(0, y, 1, 0);
-			if (result != 0) {
-				return result;
-			}
-		}
-
-		// check diagonals
-		result = check_if_same(0, 0, 1, 1);
-		if (result != 0) {
-			return result;
-		}
-		result = check_if_same(2, 0, -1, 1);
-		if (result != 0) {
-			return result;
-		}
-
-		// check for tie
-		result = -1;
-		for (int i = 0; i < 3; i++) {
-			for (int j = 0; j < 3; j++) {
-				if (board[i][j] == State::EMPTY) {
-					result = 0;
-					break;
-				}
-			}
-		}
-
-		// either tied (result = -1) or not terminal state (result = 0)
-		return result;
-	}
 	// A function to visualize the board as a 3x3 matrix with values.
 	void visualize_board() {
 		//std::cout << "Visualizing" << std::endl;
@@ -148,26 +86,68 @@ public:
 			std::cout << std::endl;
 		}
 	}
-	// A function that gets the minimax value of all moves
-	int minimax(int depth, bool toMaximize) {
-		int player = State::PLAYER_TWO;
-		int opponent = State::PLAYER_ONE;
 
-		int score = evaluate();
+	// A function to check if any player has won, or game is tied
+	int evaluate() {
+		int first_player;
 
-		// If player-1 has won
-		if (score == 1) {
-			return score;
+		// check all rows
+		for (int x = 0; x < 3; x++) {
+			first_player = board[x][0];
+			if (first_player != State::EMPTY && first_player == board[x][1] && first_player == board[x][2]) {
+				return first_player;
+			}
 		}
 
-		// If player-2 has won
+		// check all columns
+		for (int y = 0; y < 3; y++) {
+			first_player = board[0][y];
+			if (first_player != State::EMPTY && first_player == board[1][y] && first_player == board[2][y]) {
+				return first_player;
+			}
+		}
+
+		// check diagonals
+		first_player = board[0][0];
+		if (first_player != State::EMPTY && first_player == board[1][1] && first_player == board[2][2]) {
+			return first_player;
+		}
+		first_player = board[2][0];
+		if (first_player != State::EMPTY && first_player == board[1][1] && first_player == board[0][2]) {
+			return first_player;
+		}
+
+		// check for tie
+		for (int i = 0; i < 3; i++) {
+			for (int j = 0; j < 3; j++) {
+				// not a terminal state
+				if (board[i][j] == State::EMPTY) {
+					return -1;
+				}
+			}
+		}
+
+		// game tied
+		return 0;
+	}
+
+	// A function that gets the minimax value of all moves
+	int minimax(int depth, bool toMaximize) {
+		int score = evaluate();
+			
+		// If human has won
+		if (score == 1) {
+			return -10;
+		}
+
+		// If ai has won
 		if (score == 2) {
-			return score;
+			return 10;
 		}
 
 		// If game is tied 
-		if (score == -1) {
-			return score;
+		if (score == 0) {
+			return 0;
 		}
 
 		if (toMaximize) {
@@ -175,8 +155,8 @@ public:
 			for (int i = 0; i < 3; i++) {
 				for (int j = 0; j < 3; j++) {
 					if (board[i][j] == State::EMPTY) {
-						board[i][j] = player;
-						best = std::max(best, minimax(depth + 1, !toMaximize));
+						board[i][j] = ai;
+						best = std::max(best, minimax(depth + 1, false));
 						board[i][j] = State::EMPTY;
 					}
 				}
@@ -188,8 +168,8 @@ public:
 			for (int i = 0; i < 3; i++) {
 				for (int j = 0; j < 3; j++) {
 					if (board[i][j] == State::EMPTY) {
-						board[i][j] = opponent;
-						best = std::max(best, minimax(depth + 1, !toMaximize));
+						board[i][j] = human;
+						best = std::min(best, minimax(depth + 1, true));
 						board[i][j] = State::EMPTY;
 					}
 				}
@@ -197,23 +177,22 @@ public:
 			return best;
 		}
 	}
+
 	// A function tot find the best possible move for the computer
 	Move find_best_move() {
-		int player = State::PLAYER_TWO;
-		int opponent = State::PLAYER_ONE;
-
-		int best_val = -INF, move_val;
+		int best_val = -INF;
+		int move_val = -INF;
 		Move best_move;
 
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j++) {
 				if (board[i][j] == State::EMPTY) {
-					board[i][j] = player;
+					board[i][j] = ai;
 					move_val = minimax(0, false);
 					board[i][j] = State::EMPTY;
 
 					if (move_val > best_val) {
-						best_move = Move(i+1, j+1);
+						best_move = Move(i, j);
 						best_val = move_val;
 					}
 				}
@@ -222,6 +201,7 @@ public:
 
 		return best_move;
 	}
+
 	// A function to change the current turn to the other player
 	void change_chance(int &current_chance) {
 		if (current_chance == State::PLAYER_ONE) {
@@ -231,6 +211,7 @@ public:
 			current_chance = State::PLAYER_ONE;
 		}
 	}
+
 	// A function to get computer or player choice input
 	bool get_if_computer() {
 		char player_choice;
@@ -248,6 +229,7 @@ public:
 			}
 		}
 	}
+
 	// A function to get player name(s)
 	std::pair<std::string, std::string> get_player_names(bool is_computer) {
 		std::string a, b;
@@ -264,10 +246,11 @@ public:
 
 		return make_pair(a, b);
 	}
+
 	// A function to check if the game has ended
 	bool check_game_end() {
 		int result = evaluate();
-		if (result == -1) {
+		if (result == 0) {
 			visualize_board();
 			std::cout << "Game tied!" << std::endl;
 		}
@@ -285,33 +268,35 @@ public:
 
 		return true;
 	}
+
 	// The main loop of the application.
 	void play() {
 		// Setting random seed
 		srand(time(0));
 
-		int result = 0;
 		Move move;
-
 		bool is_computer = get_if_computer();
 		std::tie(player1, player2) = get_player_names(is_computer);
 
+		// Randomly deciding who startss
 		int current_chance = rand() % 2 + 1;
 		std::cout << "By random chance, " << ((current_chance == 1)? player1 : player2) << " is starting." << std::endl;
 
-		while (result == 0) {
+		while (true) {
 			visualize_board();
 			std::cout << ((current_chance == State::PLAYER_ONE)? player1 : player2) << "\'s turn: ";
 
 			if (current_chance == State::PLAYER_TWO && is_computer) {
 				move = find_best_move();
-				std::cout << "Computer plays " << move.row << " " << move.col << std::endl;
+				std::cout << "Computer plays " << move.row + 1 << " " << move.col + 1 << std::endl;
 			}
 			else {
 				move.get_input(board);
+				move.row -= 1;
+				move.col -= 1;
 			}
 
-			board[move.row - 1][move.col - 1] = current_chance;
+			board[move.row][move.col] = current_chance;
 			
 			if (check_game_end()) {
 				std::cout << "Game over!" << std::endl;
